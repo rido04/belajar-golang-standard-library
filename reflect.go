904901e71d83dffe1132e12adf340ab0772a0522
Ridho Febrian <ridhofebrian.445@gmail.com>
2025-04-03 19:04:13 +0700

    belajar library golang

---
+++ reflect.go
@@ -0,0 +1,9 @@
+package main
+
+// PACKAGE REFLECT
+// reflection digunakan untuk melihat struktur kode kita pada saat aplikasi sedang berjalan
+// di golang kita bias gunakan package reflect
+// sangat berguna untuk membuat library general sehingg mudah digunakan
+func main(){
+
+}
